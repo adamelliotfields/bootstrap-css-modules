d2d991bca786dff28dd843857af2586b60ee116c
Adam Fields <adam.elliot.fields@gmail.com>
2017-07-22 19:16:21 -0400

    Added all utilities.

--- utilities/_index.js
+++ utilities/_index.js
@@ -2,15 +2,28 @@ import align from './align.css';
 import background from './background.css';
 import borders from './borders.css';
 import clearfix from './clearfix.css';
-import close from './close.css';
 import display from './display.css';
 import flex from './flex.css';
 import float from './float.css';
+import justify from './justify.css';
+import margin from './margin.css';
+import marginBottom from './marginBottom.css';
+import marginLeft from './marginLeft.css';
+import marginRight from './marginRight.css';
+import marginTop from './marginTop.css';
+import marginX from './marginX.css';
+import marginY from './marginY.css';
+import padding from './padding.css';
+import paddingBottom from './paddingBottom.css';
+import paddingLeft from './paddingLeft.css';
+import paddingRight from './paddingRight.css';
+import paddingTop from './paddingTop.css';
+import paddingX from './paddingX.css';
+import paddingY from './paddingY.css';
 import position from './position.css';
 import responsiveEmbed from './responsiveEmbed.css';
 import screenreaders from './screenreaders.css';
 import sizing from './sizing.css';
-import spacing from './spacing.css';
 import text from './text.css';
 import visibility from './visibility.css';
 
@@ -19,15 +32,28 @@ export {
   background,
   borders,
   clearfix,
-  close,
   display,
   flex,
   float,
+  justify,
+  margin,
+  marginBottom,
+  marginLeft,
+  marginRight,
+  marginTop,
+  marginX,
+  marginY,
+  padding,
+  paddingBottom,
+  paddingLeft,
+  paddingRight,
+  paddingTop,
+  paddingX,
+  paddingY,
   position,
   responsiveEmbed,
   screenreaders,
   sizing,
-  spacing,
   text,
   visibility
 };
